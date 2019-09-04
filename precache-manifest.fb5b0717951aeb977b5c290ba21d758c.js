07bb4c88e0d420bd189e9c1e1816fc9329d23516
JacobWebster <jacob.webster@gmail.com>
2019-09-04 16:24:31 -0400

    Updates

---
+++ precache-manifest.fb5b0717951aeb977b5c290ba21d758c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3721fa8e6e311ff6c1e8980e2c91e4b",
+    "url": "/crypto-tracker/index.html"
+  },
+  {
+    "revision": "45387b1c0dbf963754d7",
+    "url": "/crypto-tracker/static/css/main.b8ebf7b0.chunk.css"
+  },
+  {
+    "revision": "3637f3fdb4b8e21495ed",
+    "url": "/crypto-tracker/static/js/2.9bb63fd2.chunk.js"
+  },
+  {
+    "revision": "45387b1c0dbf963754d7",
+    "url": "/crypto-tracker/static/js/main.10aec5af.chunk.js"
+  },
+  {
+    "revision": "b20b3e196cde2ddd24da",
+    "url": "/crypto-tracker/static/js/runtime~main.0b27a8e9.js"
+  }
+]);
